e268d39be7212264e342530efc919894fda19d86
joaovictordecamargoesilva <joaovictor.piloto110@gmail.com>
2025-11-07 16:32:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Redireciona requisições de /api para o servidor backend
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
         secure: false,
       },
